242016505696432e16b1aafd624111b00929cf6b
msiddiqi <muhammadsiddiqi@Muhammads-MacBook-Pro.local>
2017-04-22 15:29:50 -0400

    sd

---
+++ EchoServiceClient3/EchoServiceClient/src/app/services/EchoRequestService.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import 'rxjs/Rx';
+
+@Injectable()
+export class EchoRequestService {
+    sendEchoRequest(message: String): Observable<String> {
+        var httpObservable : Observable<Response>= 
+                                this._http.get('http://localhost:9000?message='+message);
+                                
+        return httpObservable.map(response => response.text());
+    }
+
+    constructor(private _http: Http) {
+    }
+}
